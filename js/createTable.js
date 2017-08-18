9c7c88f01b05455e6e9fdcdb0544195e22930308
pangc <pangc_1992@163.com>
2017-08-18 16:45:17 +0800

    Update createTable.js

--- js/createTable.js
+++ js/createTable.js
@@ -5,35 +5,35 @@
 		this.cellHeight=0;
 	}
 
-	Table.prototype={
+    Table.prototype={
 		createTable:function(json){
-			this.nodeElement.style=null;		
+		    this.nodeElement.style=null;		
 		    this.nodeElement.style.width=json.width+"px";
 		    this.nodeElement.style.height=json.height+"px";
 		    this.nodeElement.style.margin="auto";
 			
-			this.cellWidth=json.width/json.col;
-			this.cellHeight=json.height/json.row;
+		    this.cellWidth=json.width/json.col;
+		    this.cellHeight=json.height/json.row;
 			
 		    this.cssCon(); 
 			
-			for(var i = 0;i<json.row;i++)
-			{
-				var rowEle = document.createElement("div");
-				rowEle.setAttribute("contenteditable",false);
-				rowEle.className = "row";
-				this.nodeElement.appendChild(rowEle);
-				if(i%2==0){
-					rowEle.className= "doubleRow";
-			    }
-			    for(var j = 0;j<json.col;j++)
-			    {
-					var colEle = document.createElement("div");
-					colEle.setAttribute("contenteditable",true);
-					colEle.className = "col";
-					rowEle.appendChild(colEle);
-			    }
-			}
+		    for(var i = 0;i<json.row;i++)
+		    {
+			var rowEle = document.createElement("div");
+			rowEle.setAttribute("contenteditable",false);
+			rowEle.className = "row";
+			this.nodeElement.appendChild(rowEle);
+			if(i%2==0){
+				rowEle.className= "doubleRow";
+			 }
+			 for(var j = 0;j<json.col;j++)
+			  {
+				var colEle = document.createElement("div");
+				colEle.setAttribute("contenteditable",true);
+				colEle.className = "col";
+				rowEle.appendChild(colEle);
+			  }
+		     }
 		},
 		cssCon:function(){
 			var css = document.getElementsByTagName("style")[0];		
